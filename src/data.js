bdc3d5ce318c4c92e42d32bf05f4d64fcc986d6e
capprimmir <capprimmir21@gmail.com>
2021-11-08 07:54:30 -0500

    Add Slider component and data.js items list

---
+++ src/data.js
@@ -0,0 +1,76 @@
+export const sliderItems = [
+  {
+    id: 1,
+    img: "https://images.unsplash.com/photo-1578100584776-fdf3ff0b367f?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Y2FzdWFsJTIwb3V0Zml0fGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+    title: "FALL SALE",
+    desc:"GET THE NEWEST FALL SEASON STYLE! GET 30% OF NEW ARRIVALS",
+    bg:"f5fafd"
+  },
+  {
+    id: 2,
+    img:"https://images.unsplash.com/photo-1617442137585-ae532ca1645f?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzV8fHdpbnRlciUyMGZhc2hpb258ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60",
+    title:"WINTER SALE",
+    desc:"GET THE NEWEST FALL SEASON STYLE! GET 30% OF NEW ARRIVALS",
+    bg:"fcf1ed"
+  },
+  {
+    id: 3,
+    img:"https://images.unsplash.com/flagged/photo-1559502867-c406bd78ff24?ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8Y2FzdWFsJTIwb3V0Zml0fGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60",
+    title:"POPULAR SALE",
+    desc:"GET THE NEWEST FALL SEASON STYLE! GET 30% OF NEW ARRIVALS",
+    bg:"fbf0f4"
+  },
+]
+
+export const categories = [
+  {
+    id: 1,
+    img:"https://images.pexels.com/photos/1030946/pexels-photo-1030946.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    title:"SWEATER STYLES",
+  },
+  {
+    id: 2,
+    img:"https://images.pexels.com/photos/7760649/pexels-photo-7760649.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    title: "LIGHT JACKETS"
+  },
+  {
+    id: 3,
+    img:"https://images.pexels.com/photos/3951783/pexels-photo-3951783.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    title:"MUSTHAVE PIECES"
+  },
+]
+
+export const popularProducts = [
+  {
+    id: 1,
+    img:"https://images.pexels.com/photos/2755612/pexels-photo-2755612.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+  },
+  {
+    id: 2,
+    img:"https://images.pexels.com/photos/1187777/pexels-photo-1187777.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+  },
+  {
+    id: 3,
+    img:"https://images.pexels.com/photos/7828580/pexels-photo-7828580.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+  },
+  {
+    id: 4,
+    img:"https://images.pexels.com/photos/7828580/pexels-photo-7828580.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+  },
+  {
+    id: 5,
+    img:"https://images.pexels.com/photos/7828580/pexels-photo-7828580.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+  },
+  {
+    id: 6,
+    img:"https://images.pexels.com/photos/1187777/pexels-photo-1187777.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+  },
+  {
+    id: 7,
+    img:"https://images.pexels.com/photos/7828580/pexels-photo-7828580.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+  },
+  {
+    id: 8,
+    img:"https://images.pexels.com/photos/1187777/pexels-photo-1187777.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+  }
+]
